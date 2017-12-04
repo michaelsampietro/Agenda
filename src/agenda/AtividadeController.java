6fc007ff7cd311bf60bfd7272da6a3e28f5d82ad
michaelsampietro <michaelsampietro@Michaels-MacBook-Pro.local>
2017-12-04 13:26:44 -0200

    Criando service

--- src/agenda/AtividadeController.java
+++ src/agenda/AtividadeController.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class AtividadeController {
     
     AtividadeModel model;
-     AtividadeView view;
+    AtividadeView view;
     
     public AtividadeController (AtividadeModel model, AtividadeView view) { 
         this.model = model;
